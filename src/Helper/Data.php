ec306f3a08d30c5e3a69a4758cedbca855da81bd
tjeujansen <tjansen@emico.nl>
2024-12-06 09:50:47 +0100

    Get Tweakwise id from export module

--- src/Helper/Data.php
+++ src/Helper/Data.php
@@ -8,16 +8,19 @@ use Magento\Framework\App\Helper\AbstractHelper;
 use Magento\Framework\App\Helper\Context;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Store\Model\StoreManagerInterface;
+use Tweakwise\Magento2TweakwiseExport\Model\Helper;
 
 class Data extends AbstractHelper
 {
     /**
      * @param Context $context
      * @param StoreManagerInterface $storeManager
+     * @param Helper $exportHelper
      */
     public function __construct(
         Context $context,
-        private readonly StoreManagerInterface $storeManager
+        private readonly StoreManagerInterface $storeManager,
+        private readonly Helper $exportHelper
     ) {
         parent::__construct($context);
     }
@@ -26,13 +29,10 @@ class Data extends AbstractHelper
      * @param int $entityId
      * @return string
      * @throws NoSuchEntityException
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     * @SuppressWarnings(PHPMD.UnusedLocalVariable)
      */
     public function getTweakwiseId(int $entityId): string
     {
         $storeId = $this->storeManager->getStore()->getId();
-        return '100014';
-//        return $this->exportHelper->getTweakwiseId($storeId, $entityId); // TODO: require export module
+        return $this->exportHelper->getTweakwiseId($storeId, $entityId);
     }
 }
