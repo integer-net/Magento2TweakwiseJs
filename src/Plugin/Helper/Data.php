895fba031c47d16be6c4994ce4916c97384db1eb
tjeujansen <tjansen@emico.nl>
2024-12-20 12:42:53 +0100

    Prevent trigger of suggestions and catalog search page

---
+++ src/Plugin/Helper/Data.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tweakwise\TweakwiseJs\Plugin\Helper;
+
+use Magento\Search\Helper\Data as Subject;
+use Tweakwise\TweakwiseJs\Model\Config;
+use Tweakwise\TweakwiseJs\Model\Enum\SearchType;
+
+class Data
+{
+    /**
+     * @param Config $config
+     */
+    public function __construct(
+        private readonly Config $config
+    ) {
+    }
+
+    /**
+     * If search type is not the default Magento search -> disable the Magento suggestions by setting empty URL
+     * @param Subject $subject
+     * @param string $result
+     * @return string
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function afterGetSuggestUrl(Subject $subject, string $result): string
+    {
+        if ($this->config->getSearchType() === SearchType::MAGENTO_DEFAULT->value) {
+            return $result;
+        }
+
+        return '';
+    }
+}
